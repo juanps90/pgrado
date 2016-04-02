db7b39e252aa7da654b9f716947859004a68aa52
viki <elieljuegos@gmail.com>
2016-04-02 05:18:53 -0300

    modified:   learning_by_imitation/scripts/behavior/irA.py 	modified:   learning_by_imitation/scripts/maestro.py 	modified:   scenes/aprender.ttt

--- learning_by_imitation/scripts/behavior/irA.py
+++ learning_by_imitation/scripts/behavior/irA.py
@@ -31,7 +31,7 @@ class irA(comportamiento):
     delay = 0
     changeTime = None
     
-    colorValido=[Const.SENSOR_COLOR_DETECT_RED, Const.SENSOR_COLOR_DETECT_GREEN,Const.SENSOR_COLOR_DETECT_BLUE, Const.SENSOR_COLOR_DETECT_YELLOW,Const.SENSOR_COLOR_DETECT_ORANGE]
+    colorValido=[Const.SENSOR_COLOR_DETECT_ORANGE,Const.SENSOR_COLOR_DETECT_YELLOW, Const.SENSOR_COLOR_DETECT_GREEN,Const.SENSOR_COLOR_DETECT_RED,Const.SENSOR_COLOR_DETECT_BLUE]
     
     #son las distancias minimas en aprender, angulo para esa distancia
     minDist=200
@@ -58,11 +58,34 @@ class irA(comportamiento):
         return -1
         
     #recibe los datos de los sensores y verifica cual es el objeto mas cercano por el tama;o
-    def getIndObjetoMasCerca(self,data):    
+    def getIndObjMoreNear(self,data): 
+        indObj=-1
+        i = 0
+        highMax=-1
+        salida=-1
+        while i < len(data):
+            indObj=indObj+1
+            aux= data[i+2] * data[i+3] 
+            if aux>highMax:
+                salida=indObj
+                highMax=aux 
+            i = i + 4
+
+        return salida
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         highMax=-1
         ind=-1
         for d in range( len(data)-1):
-            aux= data[6*d+3]
+            aux= data[4*d+2] 
             if aux>highMax:
                 ind=d
                 highMax=aux
@@ -92,7 +115,7 @@ class irA(comportamiento):
             if auxIndColor!=-1:
                 #se pregunta si el sensor de distancia esta activo pero ademas se debe verificar que la 
                 #distancia medida sea la del objeto y no de otro para eso se usa la altura
-                if self.dataSensor.has_key(Const.SENSOR_NOSE_ULTRASONIC_ID) and self.getIndObjetoMasCerca(self.dataSensor[Const.SENSOR_VISION_HEAD_ID])==auxIndColor:                
+                if self.dataSensor.has_key(Const.SENSOR_NOSE_ULTRASONIC_ID) and self.getIndObjMoreNear(self.dataSensor[Const.SENSOR_VISION_HEAD_ID])==auxIndColor:                
                     if self.dataSensor[Const.SENSOR_NOSE_ULTRASONIC_ID][0] > distance + self.DELTA_DISTANCE:
                         self.action = self.ACTION_FORWARD
                     elif self.dataSensor[Const.SENSOR_NOSE_ULTRASONIC_ID][0] < distance - self.DELTA_DISTANCE:
@@ -208,12 +231,13 @@ class irA(comportamiento):
        noseSensor = data[Const.SENSOR_NOSE_ULTRASONIC_ID]
        
 
-       rospy.loginfo("EL COLOR EN IR_A ES " + str(headSensor[1]))
+       #rospy.loginfo("EL COLOR EN IR_A ES " + str(headSensor[1]))
        
        #esta entre los colores es la unica condicion a verificar
        #ya que se verifico que hay lectura de distancua mas arriba
-       cond0=  headSensor[1] in self.colorValido  
-       
+       aux=self.getIndObjMoreNear(headSensor)
+       cond0=  headSensor[aux*4+1] in self.colorValido  
+       rospy.loginfo("color a aprender "+str(headSensor[aux*4+1])+str(cond0))
        '''
        cond1=self.PARAM_DISTANCE - self.DELTA_DISTANCE <= noseSensor 
        cond2=noseSensor[0] <= self.PARAM_DISTANCE + self.DELTA_DISTANCE  
@@ -227,19 +251,25 @@ class irA(comportamiento):
            activate=True
            
        rospy.loginfo("Active irA" + str(activate))
-       rospy.loginfo("color = " + str(headSensor[1]) + " distancia  = " + str(noseSensor) + " angulo = " + str(headSensor[0]))
+       rospy.loginfo("color = " + str(headSensor[aux*4+1]) + " distancia  = " + str(noseSensor) + " angulo = " + str(headSensor[aux*4]))
        return activate
 
 
 
     def similarHeadSensor(self, headSens, l):
-        ret = False
+        aux =False 
+        indObj=-1
         i = 1
-        while ((not ret) and (i < len(l))):
-            ret = ret or headSens.similar([l[i - 1], l[i]])
-            i = i + 2
-        rospy.loginfo("EN SIMILAR HEAD SENSOR LOS DATOS SON " + str(l) + " Y LA SALIDA ES " + str(ret))
-        return ret
+        
+        while ((not aux) and (i < len(l))):
+            indObj=indObj+1
+            aux = aux or headSens.similar([l[i - 1], l[i]])
+            i = i + 4
+
+        rospy.loginfo("EN SIMILAR HEAD SENSOR LOS DATOS SON " + str(l) + " Y LA SALIDA ES " + str(aux))
+        if aux:
+            return indObj
+        return -1
            
     def veriPosSenEjecutar(self,data):
        activate=False
@@ -250,8 +280,9 @@ class irA(comportamiento):
        headSens = Sensores.get(Const.SENSOR_VISION_HEAD_ID,     self.parametros[Const.SENSOR_VISION_HEAD_ID])
        ultrSens = Sensores.get(Const.SENSOR_NOSE_ULTRASONIC_ID, self.parametros[Const.SENSOR_NOSE_ULTRASONIC_ID])
        #msgLight = Int32()
-
-       if self.similarHeadSensor(headSens, data[Const.SENSOR_VISION_HEAD_ID]) and ultrSens.similar(data[Const.SENSOR_NOSE_ULTRASONIC_ID]):       
+       
+       esSimiliar=self.similarHeadSensor(headSens, data[Const.SENSOR_VISION_HEAD_ID])
+       if esSimiliar==self.getIndObjMoreNear(data[Const.SENSOR_VISION_HEAD_ID])  and ultrSens.similar(data[Const.SENSOR_NOSE_ULTRASONIC_ID])   :       
            rospy.loginfo("CUMPLE POSTCONDICION IR_A PARA " + str(self.identify) + " CON DISTANCIA " + str(self.parametros[Const.SENSOR_NOSE_ULTRASONIC_ID]) + ", COLOR Y ANGULO " + str(self.parametros[Const.SENSOR_VISION_HEAD_ID]))
            #msgLight.data = [self.identify,1]
            #self.light.publish(msgLight)
@@ -283,10 +314,11 @@ class irA(comportamiento):
             outProxim=str (Const.SENSOR_NOSE_ULTRASONIC_ID) + "#" +str(self.minDist) 
         if data.has_key(Const.SENSOR_VISION_HEAD_ID):
             head=data[Const.SENSOR_VISION_HEAD_ID]
+            idObj=self.getIndObjMoreNear(head)
             #si es la minima distancia se guarda el angulo
             if esDistMin:
-                self.angMinDist=head[0]
-            outHead=str (Const.SENSOR_VISION_HEAD_ID) + "#" +str(self.angMinDist) + "#" +str(head[1]) 
+                self.angMinDist=head[4*idObj]
+            outHead=str (Const.SENSOR_VISION_HEAD_ID) + "#" +str(self.angMinDist) + "#" +str(head[4*idObj+1]) 
 
         salida=""
         if len(outHead)>0:
